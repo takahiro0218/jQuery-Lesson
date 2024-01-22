3a0f9dcb2b9b70981dab1db5624803d2595bccfb
takahiro0218 <ashitaka1259@gmail.com>
2024-01-22 18:48:15 +0900

    jQuery-lesson2

--- app.js
+++ app.js
@@ -1,3 +1,30 @@
-$(document).ready(function () {
-  $('body').html('<h1>Hello jQuery!!</h1>');
+// $(document).ready(function () {
+//   $('body').html('<h1>Hello jQuery!!</h1>');
+// });
+
+// $(function(){
+//   $('.box1').css({
+//     'background-color': '#0000FF',
+//     'height': '100px'
+//   });
+// });
+
+// $(function(){
+//   $('.box1').show();
+//   $(".box1").css({"background-color": "#0000FF"});
+// });
+
+// $(function(){
+//     $(".box1").hide();
+// });
+
+$(function(){
+  $(".box1").slideDown(1000,function(){
+    $(".box1").css({
+      "height": "100px",
+      "width": "200px",
+      "background-color": "#0000FF"
+    });
+    $(".box1").slideUp(1000);
+  })
 });
