cf73541a05893ca74667f4909f0cd04a2691fe47
takahiro0218 <ashitaka1259@gmail.com>
2024-01-23 23:09:38 +0900

    jQuery-lesson3

--- app.js
+++ app.js
@@ -18,13 +18,70 @@
 //     $(".box1").hide();
 // });
 
+// $(function(){
+//   $(".box1").slideDown(1000,function(){
+//     $(".box1").css({
+//       "height": "100px",
+//       "width": "200px",
+//       "background-color": "#0000FF"
+//     });
+//     $(".box1").slideUp(1000);
+//   })
+// });
+
+// $(function(){
+//   $(".box1").mouseover(function(){
+//     $(".box1").css({"background-color": "#0000FF"});
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").css({"background-color": "#FF0000"});
+//   });
+// });
+
+// $(function(){
+//   $(".box1").mouseover(function(){
+//     $(".box1").addClass("box1-ext");
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").removeClass("box1-ext");
+//   });
+// });
+
+// $(function(){
+//   $(".box1").on("click", function(){
+//     $(".box1").addClass("box1-ext");
+//   });
+//   $(".box1").mouseout(function(){
+//     $(".box1").removeClass("box1-ext");
+//   });
+// });
+
+// $(function(){
+//   $('.bg1').on('click', function(){
+//     $('.bg1').slideUp();
+//   });
+
+//   $('.bg2').on('click', function(){
+//     $('.bg2').slideUp();
+//   });
+
+//   $('.bg3').on('click', function(){
+//     $('.bg3').slideUp();
+//   });
+
+//   $('.bg4').on('click', function(){
+//     $('.bg4').slideUp();
+//   });
+// });
+
+// $(function(){
+//   $(".box1").on("click", function(){
+//     $(this).slideUp();
+//   });
+// });
+
 $(function(){
-  $(".box1").slideDown(1000,function(){
-    $(".box1").css({
-      "height": "100px",
-      "width": "200px",
-      "background-color": "#0000FF"
-    });
-    $(".box1").slideUp(1000);
-  })
+  $("button").on("click", function(){
+    $("ul").children().css("color", "red");
+  });
 });
